27c13ca5589fe192d4d73d29e095eb6c2bcca325
VorotySergey <95651513+SergeyVorotiy@users.noreply.github.com>
2022-10-11 22:12:25 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: '/src/index.js',
+    output: {
+        filename: "main.js"
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./src/index.pug",
+            filename: "index.html"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        esModule: true
+                    }
+                }, 'css-loader'],
+                test: /\.css$/
+            },
+            {
+                test: /\.pug$/,
+                use: ['pug-loader']
+            }
+        ]
+    }
+}
